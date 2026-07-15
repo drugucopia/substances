8e7bbc3e192348d50c204ef0988d2040deb9b308
conflict <conflict@cocaine.ninja>
2026-07-15 14:55:10 -0400

    add comeup time to kratom

--- opioids/kratom.ts
+++ opioids/kratom.ts
@@ -1,5 +1,5 @@
 // Auto-generated from substances.txt
-import type { Substance } from '../types';
+import type { Substance } from "../types";
 
 // Substance Data
 // Name: Kratom
@@ -7,32 +7,29 @@ import type { Substance } from '../types';
 // Categories: stimulants, opioids
 // Class: Indole alkaloids
 
-export const kratom: Substance = 
-{
-  "id": "kratom",
-  "name": "Kratom",
-  "commonNames": [
+export const kratom: Substance = {
+  id: "kratom",
+  name: "Kratom",
+  commonNames: [
     "Mitragyna Speciosa",
     "กระท่อม (Thai)",
     "Ketum",
     "Kratom",
-    "Kratum"
+    "Kratum",
   ],
-  "categories": [
-    "stimulants",
-    "opioids"
-  ],
-  "class": "Indole alkaloids",
-  "description": "Mitragyna speciosa (also known as kratom) is a tropical tree of the coffee family indigenous to South East Asia. M. speciosa is indigenous to Thailand, Indonesia, Malaysia, Myanmar, and Papua New Guinea, where it has been used in traditional medicines for centuries. Starting in the 2000s, kratom began to receive significant attention due to increased use in Western cultures as an alternative medicine. Lower doses are generally reported to produce caffeine-like stimulant effects such as enhanced focus and motivation while higher doses produce typical opioid effects like pain relief, sedation, and euphoria.",
-  "effects": {
-    "positive": [
+  categories: ["stimulants", "opioids"],
+  class: "Indole alkaloids",
+  description:
+    "Mitragyna speciosa (also known as kratom) is a tropical tree of the coffee family indigenous to South East Asia. M. speciosa is indigenous to Thailand, Indonesia, Malaysia, Myanmar, and Papua New Guinea, where it has been used in traditional medicines for centuries. Starting in the 2000s, kratom began to receive significant attention due to increased use in Western cultures as an alternative medicine. Lower doses are generally reported to produce caffeine-like stimulant effects such as enhanced focus and motivation while higher doses produce typical opioid effects like pain relief, sedation, and euphoria.",
+  effects: {
+    positive: [
       "Cognitive euphoria - In comparison to other opioids, kratom can be considered as less intense in its cognitive euphoria when compared with that of morphine or heroin.",
       "Empathy, affection, and sociability enhancement",
       "Increased music appreciation",
       "Focus enhancement - Focus enhancement occurs at lower doses and disappears as the dose is increased and the sedative and euphoric effects take over.",
-      "Motivation enhancement"
+      "Motivation enhancement",
     ],
-    "neutral": [
+    neutral: [
       "Visual disconnection - A sense of subtle disconnection from visual input is often experienced with high doses of kratom. This is similar to a low-level version of the same effect universally experienced with dissociatives and may be a result of NMDA antagonist properties of rhynchophylline, an alkaloid found in kratom.",
       "Dream potentiation",
       "Thought acceleration",
@@ -41,35 +38,33 @@ export const kratom: Substance =
       "Double vision",
       "Visual acuity suppression",
       "Anxiety suppression",
-      "Sleepiness - Certain strains of kratom are noted to sometimes make users very tired. In high dosages this can get to the point where the user may be drifting in and out of consciousness or even experience what is called \\\"nodding\\\".",
-      "Internal hallucination - The internal hallucinations of kratom can be described as more solid than psychedelics and do not seem to be composed of visual geometry. They typically manifest themselves through hypnagogic scenarios. They are most common with high doses and can be comprehensively described in their variations as lucid in believability, fixed in style, autonomous in controllability, and equal in new experiences and memory replays in content."
+      'Sleepiness - Certain strains of kratom are noted to sometimes make users very tired. In high dosages this can get to the point where the user may be drifting in and out of consciousness or even experience what is called \\"nodding\\".',
+      "Internal hallucination - The internal hallucinations of kratom can be described as more solid than psychedelics and do not seem to be composed of visual geometry. They typically manifest themselves through hypnagogic scenarios. They are most common with high doses and can be comprehensively described in their variations as lucid in believability, fixed in style, autonomous in controllability, and equal in new experiences and memory replays in content.",
     ],
-    "negative": [
-      "Irritability"
-    ]
+    negative: ["Irritability"],
   },
-  "defaultUnit": "g",
-  "routeData": {
-    "oral": {
-      "dosage": {
-        "threshold": "1g",
-        "light": "2-3g",
-        "common": "3-5g",
-        "strong": "5-8g",
-        "heavy": "8g"
+  defaultUnit: "g",
+  routeData: {
+    oral: {
+      dosage: {
+        threshold: "1g",
+        light: "2-3g",
+        common: "3-5g",
+        strong: "5-8g",
+        heavy: "8g",
       },
-      "duration": {
-        "onset": "20-40 minutes",
-        "comeup": "Unknown",
-        "peak": "1-2 hours",
-        "offset": "3-6 hours",
-        "total": "2-4 hours",
-        "afterglow": "Unknown"
-      }
-    }
+      duration: {
+        onset: "20-40 minutes",
+        comeup: "20-90 minutes",
+        peak: "1-2 hours",
+        offset: "3-6 hours",
+        total: "2-4 hours",
+        afterglow: "Unknown",
+      },
+    },
   },
-  "interactions": {
-    "dangerous": [
+  interactions: {
+    dangerous: [
       "Alcohol",
       "Benzodiazepines",
       "DXM",
@@ -78,20 +73,13 @@ export const kratom: Substance =
       "Ketamine",
       "MXE",
       "Tramadol",
-      "Grapefruit"
+      "Grapefruit",
     ],
-    "unsafe": [],
-    "uncertain": [
-      "MAOIs",
-      "Nitrous",
-      "PCP",
-      "Stimulants"
-    ],
-    "crossTolerances": [
-      "opioids"
-    ]
+    unsafe: [],
+    uncertain: ["MAOIs", "Nitrous", "PCP", "Stimulants"],
+    crossTolerances: ["opioids"],
   },
-  "harmReduction": [
+  harmReduction: [
     "Kratom has a low toxicity relative to dose. Like most opioids, safe usage of kratom is not known to cause any dangerous long-term complications. Heavy dosages of kratom can result in increased respiratory depression, leading onto fatal or dangerous levels of anoxia (oxygen deprivation). This is significantly less powerful than the respiratory depression of heroin or morphine. This occurs because the breathing reflex is suppressed by agonism of µ-opioid receptors proportional to the dosage consumed.",
     "It is likely impossible to achieve this using kratom in its standard leaf form as the nausea ceiling makes it difficult to consume high enough dosages without vomiting. A pure extract or tincture, however, may be potent enough to cause lethal respiratory depression at appropriate dosages, although oral administration of pure mitragynine to mice in dosages up to 920 mg/kg did not produce lethal respiratory depression. However, kratom can be fatal when it is combined with other depressants such as alcohol or benzodiazepines.",
     "Side effects associated with chronic kratom use include loss of appetite and weight loss, constipation, decreased libido, apathy, and darkening of the skin color of the face. Chronic use has been associated with bowel obstruction.",
@@ -101,18 +89,20 @@ export const kratom: Substance =
     "As with other opioids, the kratom produces dependence with chronic use and has a high abuse potential. When dependence has developed, cravings and may occur if a person suddenly stops their usage.",
     "Tolerance to many of the effects of kratom develops with prolonged and repeated use. The rate at which this occurs develops at different rates for different effects, with tolerance to the constipation-inducing effects developing particularly slowly for instance. This results in users having to administer increasingly large doses to achieve the same effects. After that, it takes about 3 - 7 days for the tolerance to be reduced to half and 1 - 2 weeks to be back at baseline (in the absence of further consumption). Kratom presents cross-tolerance with all other opioids, meaning that after the consumption of kratom all opioids will have a reduced effect.",
     "Chronic kratom use may lead to tolerance to the effects of serotonergic psychedelics due to kratom's antagonism of the 5-HT2A receptor and said receptor's paradoxical down-regulation in response to inhibitors.",
-    "Since kratom is classified as an herbal supplement and remains legal in many states, a subset of individuals mistakenly assume that it can be used regularly and eventually discontinued without any withdrawal symptoms.  In reality, most individuals who use kratom on a regular basis will experience some withdrawal symptoms following cessation."
+    "Since kratom is classified as an herbal supplement and remains legal in many states, a subset of individuals mistakenly assume that it can be used regularly and eventually discontinued without any withdrawal symptoms.  In reality, most individuals who use kratom on a regular basis will experience some withdrawal symptoms following cessation.",
   ],
-  "legality": "Legal status varies by jurisdiction. (US: not federally scheduled, banned in 6 states; UK: Psychoactive Substances Act; EU: varies by country; CA: uncontrolled; AU: Schedule 9; banned in Thailand, Malaysia, Myanmar, Denmark, Poland, Sweden, Romania)",
-  "chemistry": {
-    "formula": "Variable (plant mixture: Mitragyna speciosa - mitragynine, 7-hydroxymitragynine + other alkaloids)",
-    "molecularWeight": "N/A (plant mixture)",
-    "class": "Indole alkaloids"
+  legality:
+    "Legal status varies by jurisdiction. (US: not federally scheduled, banned in 6 states; UK: Psychoactive Substances Act; EU: varies by country; CA: uncontrolled; AU: Schedule 9; banned in Thailand, Malaysia, Myanmar, Denmark, Poland, Sweden, Romania)",
+  chemistry: {
+    formula:
+      "Variable (plant mixture: Mitragyna speciosa - mitragynine, 7-hydroxymitragynine + other alkaloids)",
+    molecularWeight: "N/A (plant mixture)",
+    class: "Indole alkaloids",
   },
-    "aliases": [],
-"history": null,
-  "afterEffects": "See route data for afterglow information.",
-  "riskLevel": "low",
+  aliases: [],
+  history: null,
+  afterEffects: "See route data for afterglow information.",
+  riskLevel: "low",
   wikipediaUrl: "https://en.wikipedia.org/wiki/Kratom",
-  "psychonautWikiUrl": "https://psychonautwiki.org/wiki/Kratom"
+  psychonautWikiUrl: "https://psychonautwiki.org/wiki/Kratom",
 };
